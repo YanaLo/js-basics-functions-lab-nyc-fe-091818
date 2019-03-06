696c01f8c05e56c4d69588cbddbce299d4f5dd4c
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-06 05:47:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const block =264
+const hq=42
+function distanceFromHqInBlocks(currentBlock){
+  if(currentBlock>hq){
+    return currentBlock-hq
+  }else{
+    return hq-currentBlock
+  }
+}
+function distanceFromHqInFeet (currentBlock){
+  let numberOfBlocks
+  if(currentBlock>hq){
+   numberOfBlocks=currentBlock-hq
+  }else{
+     numberOfBlocks= hq-currentBlock
+  }
+  return numberOfBlocks*block
+}
+function distanceTravelledInFeet(block1,block2){
+  // 43
+  // 1360
+  // 1.Want to get distance between blocks
+  const numberOfBlocks=Math.abs(block1-block2)
+  return numberOfBlocks*block
+}
+  function calculatesFarePrice(startingBlock,destinationBlock){
+    const numberOfBlocks=Math.abs(startingBlock-destinationBlock)
+    const numberOfFeet=numberOfBlocks*block
+    if(numberOfFeet<=400){
+      return 0;
+    }else if(numberOfFeet>400 &&numberOfFeet<=2000){
+      return (numberOfFeet- 400)*.02
+    }else if(numberOfFeet>2000 &&numberOfFeet<=2500  ){
+      return 25
+    }else{
+     return "cannot travel that far"
+    }
+  }
